9bf2767cfe3260a17a62dd6970addd1362d10868
79098 <ilya.nesterov1520@gmail.com>
2023-05-15 16:43:53 +1000

    hw done

---
+++ src/MyNameOnUnicode.java
@@ -0,0 +1,8 @@
+public class MyNameOnUnicode {
+    public static void main(String[] args) {
+        System.out.print('\u0399');
+        System.out.print('\u039B');
+        System.out.print('\u03B3');
+        System.out.print('\u03B1');
+    }
+}
